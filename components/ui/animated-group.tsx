7a01e859cd38da257458fb84dfaa84f0dc29df7e
Mahadsid <muhammadmahad0808@gmail.com>
2026-03-19 21:17:26 +0530

    chore: deployment check again

--- components/ui/animated-group.tsx
+++ components/ui/animated-group.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ReactNode } from 'react';
+import { JSX, ReactNode } from 'react';
 import { motion, Variants } from 'motion/react';
 import React from 'react';
 
@@ -116,11 +116,11 @@ function AnimatedGroup({
   const itemVariants = variants?.item || selectedVariants.item;
 
   const MotionComponent = React.useMemo(
-    () => motion.create(as as string),
+    () => motion.create(as as keyof JSX.IntrinsicElements),
     [as]
   );
   const MotionChild = React.useMemo(
-    () => motion.create(asChild as string),
+    () => motion.create(asChild as keyof JSX.IntrinsicElements),
     [asChild]
   );
 
